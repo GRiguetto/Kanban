697db42acd7ffcf7dcd5ab5359ff4b7c51289d51
Gabriel Riguetto <gabrielriguetto3@gmail.com>
2025-07-11 20:59:31 -0300

    Update api.spec.ts

--- kanban-frontend/src/app/services/api.spec.ts
+++ kanban-frontend/src/app/services/api.spec.ts
@@ -1,13 +1,17 @@
 import { TestBed } from '@angular/core/testing';
+import { ApiService } from './api';
 
-import { Api } from './api';
+// Corrigido para importar o nome correto: ApiService
 
-describe('Api', () => {
-  let service: Api;
+// Corrigido para descrever o teste correto
+describe('ApiService', () => {
+  // Corrigido para declarar a variável com o tipo correto
+  let service: ApiService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(Api);
+    // Corrigido para injetar o serviço correto
+    service = TestBed.inject(ApiService);
   });
 
   it('should be created', () => {
